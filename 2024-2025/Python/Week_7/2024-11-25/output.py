0eb879e4b396314ae7db3837d74c6181ed49984d
342351 <aabdullahi13@bostonk12.org>
2024-11-26 10:24:28 -0500

    Sigma

---
+++ 2024-2025/Python/Week_7/2024-11-25/output.py
@@ -0,0 +1,10 @@
+#Abdullahi Yahye
+#11/25/2024
+
+print("Abdullahi")
+print("Yahye")
+print("Abdullahi")
+print("Abdullahi","Yahye","Abdullahi")
+print("Abdullahi","Yahye","Abdullahi",end=".\n")
+print("Abdullahi","Yahye","Abdullahi",sep="#",end="*\n")
+print("Abdullahi","Yahye","Abdullahi",sep="#",end=".\n")
