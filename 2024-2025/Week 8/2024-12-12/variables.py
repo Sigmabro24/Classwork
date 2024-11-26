a327a19e5706cea20c185cb3f089afc69250deea
342351 <aabdullahi13@bostonk12.org>
2024-11-26 10:48:28 -0500

    Classwork for 11/24-11/25

---
+++ 2024-2025/Week 8/2024-12-12/variables.py
@@ -0,0 +1,24 @@
+# Abdullahi Yahye Abdullahi
+# 2024-12-12
+
+# Input, variables and types.
+
+name = input("Enter your name:")
+print(name)
+# this type is a string
+
+age = input("Enter your age:")
+print(age)
+# this is a string
+
+age = int(age)
+print(type(age))
+# this is an integer
+
+gpa = input("Enter your GPA:")
+print(gpa)
+# this is a string
+
+print("I am",name,"and I am",age ,"years old.","My GPA is",gpa)
+
+      
