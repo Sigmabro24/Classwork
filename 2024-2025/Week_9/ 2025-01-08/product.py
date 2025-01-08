8d06a4e9c4452a32fedc65576b4156eacf5d84d1
342351 <aabdullahi13@bostonk12.org>
2025-01-08 09:31:16 -0500

    Classwork for 11/24-11/25

---
+++ 2024-2025/Week_9/ 2025-01-08/product.py
@@ -0,0 +1,10 @@
+#Abduallahi
+#1/8/25
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second name:"))
+num3 = int(input("Enter third number:"))
+
+product = num1 * num2 * num3
+print()
+print(num1,"x",num2,"x",num3,"=",product)
---
+++ 2024-2025/Week_9/2025-01-07/sum.py
@@ -0,0 +1,8 @@
+# Abdullahi
+# 1/7/25
+
+num1 = int(input("Please enter a number:"))
+num2 = int(input("Please enter another number:"))
+
+sum = (num1 + num2)
+print("The sum of",num1,"and",num2,"is", sum)
