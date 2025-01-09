8ea5ad631cf011aa8abac7f8c46ea7b0394ca78e
342351 <aabdullahi13@bostonk12.org>
2025-01-09 10:11:14 -0500

    classwork

---
+++ 2024-2025/Python/Week_9/2025-01-09/if.py
@@ -0,0 +1,10 @@
+#Abdullahi
+#1/9/25
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+if num1 > num2:
+    print("The biggest number is",num1)
+else:
+    print("The biggest number is",num2)
