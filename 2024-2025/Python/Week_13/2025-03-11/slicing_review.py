1528d663a27a71af3424e77aad469a0d0c38670c
342351 <aabdullahi13@bostonk12.org>
2025-03-11 09:37:33 -0400

    classwork

---
+++ 2024-2025/Python/Week_13/2025-03-11/slicing_review.py
@@ -0,0 +1,8 @@
+#Abdullahi Yahye
+#3/11/25
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+print("")
+print("Look what I did to your name!")
+print(first_name[::-1],last_name[::-1])
