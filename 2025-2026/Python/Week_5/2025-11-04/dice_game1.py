f4adb1a7a8f14bb06a784b77f227d60deeb6236c
342351 <aabdullahi13@bostonk12.org>
2025-11-04 10:52:47 -0500

    dice with for i in range

---
+++ 2025-2026/Python/Week_5/2025-11-04/dice_game1.py
@@ -0,0 +1,14 @@
+# Abdullahi Yahye
+# 11/4/25
+def main():
+    import random
+    x= int(input("How many times would you like to roll a single sided die?"))
+    for i in range(x):print("You rolled a " + str(random.randint(1,6)))
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
