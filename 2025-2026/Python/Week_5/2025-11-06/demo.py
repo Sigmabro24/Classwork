b4985851e6f0027ef5fe0d3bc284a618b3b5faed
342351 <aabdullahi13@bostonk12.org>
2025-11-06 10:51:25 -0500

    practice test part 1

---
+++ 2025-2026/Python/Week_5/2025-11-06/demo.py
@@ -0,0 +1,26 @@
+#Abdullahi Yahye
+#2025/11/6
+def main():
+    name = "Abdullahi"
+    age = 16
+    print("Mr O looks like Tung Tung Sahur")
+    brainrot = input("What is your favorite brainrot?:")
+    print(name)
+    print(age)
+    print(brainrot)
+    num1 = -67
+    num2 = -67
+    num3 = 0
+    num4 = 0
+    num5 = 300
+    num6 = 300
+    num7 = num1
+    num8 = num4
+    num9 = num6
+    num10 = num5
+    print(id(num1))
+    print(id(num3))
+    print(id(num5))
+
+if __name__ == '__main__':
+    main()
